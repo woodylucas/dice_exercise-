36652f2c77f63fe72abf4c46eef497cc6c598ebd
woodylucas <wlucas0311@gmail.com>
2020-10-30 13:42:24 -0400

    added dice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Die from './Die'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face="five"/>
+      <Die face="six"/>
+      <Die face="three"/>
+      <Die face="two"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Die.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Die.css'; 
+
+class Die extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = {}; 
+    }
+    render() {
+        return  <i className={`fas fa-dice-${this.props.face}`}/>; 
+    }
+}
+
+export default Die; 
