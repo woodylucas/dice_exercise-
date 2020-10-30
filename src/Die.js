33fb8930730fafabfa72c57192de6d1f91d3d765
woodylucas <wlucas0311@gmail.com>
2020-10-30 16:49:22 -0400

    refactor Die component

--- src/Die.js
+++ src/Die.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import './Die.css'; 
 
 class Die extends Component {
-    constructor(props) {
-        super(props); 
-        this.state = {}; 
-    }
     render() {
         return  <i className={`Die fas fa-dice-${this.props.face}`}/>; 
     }
